d95f0e665c7b755eae8f99d9c48c35eada9db5db
iankohl <ianvictor181099@gmail.com>
2023-09-26 16:48:32 -0300

    create file extract data

---
+++ extract_data.py
@@ -0,0 +1,35 @@
+import string
+import os
+
+alfabeto = list(string.ascii_uppercase)
+
+
+nome_arquivo = "letter-recognition.txt"
+
+matriz = []
+
+with open(nome_arquivo, "r") as arquivo:
+    for linha in arquivo:
+        linha = linha.strip()
+        
+        matriz.append(linha)
+
+
+caminhoDaPasta='./letras'
+
+
+for indexLetra, letra in enumerate(alfabeto):
+    countLinha = 0
+    for indexLinha, linha in enumerate(matriz):
+        if linha[0] == letra:
+            arrayLinha = linha[2:]
+
+            while countLinha <= 100:
+                nome_arquivo = letra + '_' + str(countLinha) + ".txt"
+                caminho_completo = os.path.join(caminhoDaPasta, nome_arquivo)
+                with open(caminho_completo, "w") as arquivo:
+                    arquivo.write(arrayLinha)
+                countLinha += 1
+
+        
+
